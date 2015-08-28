a228a9fd557cd59ff1a3fe4971ef107c173a04d5
uniqueSkeeter <wenrui199231@gmail.com>
2015-08-28 19:30:28 +0800

    gg

--- src/translatehelper/actions/SampleAction.java
+++ src/translatehelper/actions/SampleAction.java
@@ -32,7 +32,7 @@ public class SampleAction implements IWorkbenchWindowActionDelegate {
 		MessageDialog.openInformation(
 			window.getShell(),
 			"TranslateHelper",
-			"Hello, Eclipse world");
+			"Hello, Eclipssssse world");
 	}
 
 	/**
